a9a09e9071be09fb88bbf903d7fb113e35c1c1de
M4TH3US17 <matheusdalvino50@gmail.com>
2023-11-27 15:15:48 -0400

    Option Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,36 @@ import 'package:flutter/material.dart';
 void main(List<String> args) => runApp(QuizAppHome());
 
 class QuizAppHome extends StatelessWidget {
+
+  final List<String> perguntas = ['Pergunta 1', 'Pergunta 2'];
+
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
         title: Text('Quiz App'),
       ),
-      body: Text('Components'),
+      body: Column(
+        children: [
+          Text(perguntas[0]),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Resposta 1'),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text('Resposta 2'),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text('Resposta 3'),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+          ),
+        ],
+      ),
     ),);
   }
 }
